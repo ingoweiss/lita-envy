3d751e6eada0f096a14f9428c6cac2819bf5500d
Ingo Weiss <ingo.weiss@lab49.com>
2016-08-01 11:56:48 -0400

    add route and method for 'stop using env [ENV_ID]'

--- lib/lita/handlers/envy.rb
+++ lib/lita/handlers/envy.rb
@@ -2,11 +2,18 @@ module Lita
   module Handlers
     class Envy < Handler
 
-      route /\Astarted using env ([A-Za-z0-9_]+)\Z/,  :start_using_environment, help:  { "started using env [ENV ID]"  => "Notifies bot that you started using environment"}, command: true
+      route /\Astarted using env ([A-Za-z0-9_]+)\Z/,  :start_using_environment, help:  { "started using env [ENV ID]"  => "Mark environment as in use by you"}, command: true
+      route /\Astopped using env ([A-Za-z0-9_]+)\Z/,  :stop_using_environment, help:  { "stopped using env [ENV ID]"  => "Mark environment as available"}, command: true
 
       def start_using_environment(response)
         env_id = response.matches.first.first
         redis.hset(['environments', env_id].join(':'), 'user', response.user.name)
+        response.reply('ok')
+      end
+
+      def stop_using_environment(response)
+        env_id = response.matches.first.first
+        redis.hset(['environments', env_id].join(':'), 'user', nil)
         response.reply('ok')
       end
 
--- spec/lita/handlers/envy_spec.rb
+++ spec/lita/handlers/envy_spec.rb
@@ -5,20 +5,35 @@ describe Lita::Handlers::Envy, lita_handler: true do
   describe 'routing' do
 
     it { is_expected.to route_command("started using env ENV123").to(:start_using_environment)  }
+    it { is_expected.to route_command("stopped using env ENV123").to(:stop_using_environment)  }
 
   end
 
   describe 'start using environment' do
 
-    it "should record environment usage" do
+    it "should mark environment as in use" do
       carl = Lita::User.create(123, name: "Carl")
       send_command('started using env ENV123', :as => carl)
       expect(subject.redis.hget('environments:ENV123', 'user')).to eq("Carl")
     end
 
     it "should confirm" do
-      carl = Lita::User.create(123, name: "Carl")
-      send_command('started using env ENV123', :as => carl)
+      send_command('started using env ENV123')
+      expect(replies.first).to eq("ok")
+    end
+
+  end
+
+  describe 'stop using environment' do
+
+    it "should mark environment as available" do
+      subject.redis.hset('environments:ENV123', 'user', 'Alicia')
+      send_command('stopped using env ENV123')
+      expect(subject.redis.hget('environments:ENV123', 'user')).to be_empty
+    end
+
+    it "should confirm" do
+      send_command('stopped using env ENV123')
       expect(replies.first).to eq("ok")
     end
 
