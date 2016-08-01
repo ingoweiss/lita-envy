e84f70eecf9db22a7319a9ed94c946a7fdd72587
Ingo Weiss <ingo.weiss@lab49.com>
2016-08-01 11:44:06 -0400

    add route and method for 'start using env [ENV_ID]'

--- lib/lita/handlers/envy.rb
+++ lib/lita/handlers/envy.rb
@@ -1,7 +1,14 @@
 module Lita
   module Handlers
     class Envy < Handler
-      # insert handler code here
+
+      route /\Astarted using env ([A-Za-z0-9_]+)\Z/,  :start_using_environment, help:  { "started using env [ENV ID]"  => "Notifies bot that you started using environment"}, command: true
+
+      def start_using_environment(response)
+        env_id = response.matches.first.first
+        redis.hset(['environments', env_id].join(':'), 'user', response.user.name)
+        response.reply('ok')
+      end
 
       Lita.register_handler(self)
     end
--- spec/lita/handlers/envy_spec.rb
+++ spec/lita/handlers/envy_spec.rb
@@ -1,4 +1,27 @@
 require "spec_helper"
 
 describe Lita::Handlers::Envy, lita_handler: true do
+
+  describe 'routing' do
+
+    it { is_expected.to route_command("started using env ENV123").to(:start_using_environment)  }
+
+  end
+
+  describe 'start using environment' do
+
+    it "should record environment usage" do
+      carl = Lita::User.create(123, name: "Carl")
+      send_command('started using env ENV123', :as => carl)
+      expect(subject.redis.hget('environments:ENV123', 'user')).to eq("Carl")
+    end
+
+    it "should confirm" do
+      carl = Lita::User.create(123, name: "Carl")
+      send_command('started using env ENV123', :as => carl)
+      expect(replies.first).to eq("ok")
+    end
+
+  end
+
 end
